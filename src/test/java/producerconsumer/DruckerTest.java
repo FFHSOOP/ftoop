54e5fb56a2cdc0172a569bd1a667f154ed1ef720
MW_01 <marco.wyssmann@students.ffhs.ch>
2018-02-21 23:09:01 +0100

    Testfolder im Projekt eingefügt und pseudo DruckerTest klasse erstellt

---
+++ src/test/java/producerconsumer/DruckerTest.java
@@ -0,0 +1,14 @@
+package producerconsumer;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DruckerTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
