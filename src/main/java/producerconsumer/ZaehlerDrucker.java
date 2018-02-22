45ab1e763b18a66902a88b1800a1a9ac3883d79e
MW_01 <marco.wyssmann@students.ffhs.ch>
2018-02-22 01:12:12 +0100

    ein paar falsche Eingaben abgefangen

--- src/main/java/producerconsumer/ZaehlerDrucker.java
+++ src/main/java/producerconsumer/ZaehlerDrucker.java
@@ -8,24 +8,43 @@ package producerconsumer;
  */
 public class ZaehlerDrucker {
 
+    static int wertMin;
+    static int wertMax;
+
     public static void main(String[] args) throws InterruptedException {
-        if (args.length != 2) {
-            System.out.println("Usage: ZaehlerDrucker <min> <max>");
-            System.exit(1);
-        }
+	if (args.length != 2) {
+	    System.out.println("Usage: ZaehlerDrucker <min> <max>");
+	    System.exit(1);
+	}
 
-        Speicher s = new Speicher();
-        Drucker d = new Drucker(s);
-        Zaehler z = new Zaehler(s, Integer.parseInt(args[0]),
-                Integer.parseInt(args[1]));
+	try {
+	    wertMin = Integer.parseInt(args[0]);
+	    wertMax = Integer.parseInt(args[1]);
+	} catch (NumberFormatException e) {
+	    System.out.println("Bitte nur Zahlen eingeben!");
+	    System.exit(1);
+	}
 
-        z.start();
-        d.start();
+	if (wertMin < 0 || wertMax < 0) {
+	    System.out.println("Bitte nur positive Zahlen eingeben!");
+	    System.exit(1);
+	}
 
-        // bissi warten, damit der Test funktioniert
-        int max = Integer.parseInt(args[1]) * 200;
-        Thread.sleep(max);
+	if (wertMin > wertMax) {
+	    System.out.println("Die erste Zahl muss keiner sein!");
+	    System.exit(1);
+	}
 
+	Speicher s = new Speicher();
+	Drucker d = new Drucker(s);
+	Zaehler z = new Zaehler(s, ZaehlerDrucker.wertMin, wertMax);
+
+	z.start();
+	d.start();
+
+	// bissi warten, damit der Test funktioniert
+	int max = wertMax * 200;
+	Thread.sleep(max);
 
     }
 
