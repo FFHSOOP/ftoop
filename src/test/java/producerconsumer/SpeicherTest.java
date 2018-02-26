b8fb7718c0675da7839093b071c5fa0dd739558c
MW_01 <marco.wyssmann@students.ffhs.ch>
2018-02-26 22:40:17 +0100

    Eine Testklasse für den Speicher, funktioniert aber noch nicht ganz.

--- src/test/java/producerconsumer/SpeicherTest.java
+++ src/test/java/producerconsumer/SpeicherTest.java
@@ -1,6 +1,4 @@
-/**
- * 
- */
+
 package producerconsumer;
 
 import static org.junit.Assert.*;
@@ -8,25 +6,26 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 /**
- * 
+ * Tests zur Funktionsweise der Speicher Klasse
  *
  */
 public class SpeicherTest {
 
-    /**
-     * Test method for {@link producerconsumer.Speicher#getWert()}.
-     */
-    @Test
-    public void testGetWert() {
-	// fail("Not yet implemented");
-    }
+    Speicher speicher = new Speicher();
 
-    /**
-     * Test method for {@link producerconsumer.Speicher#setWert(int)}.
-     */
-    @Test
-    public void testSetWert() {
-	// fail("Not yet implemented");
+    public class Runner implements Runnable {
+
+	int tempWert;
+
+	public int getTempWert() {
+	    return tempWert;
+	}
+
+	public void run() {
+
+	    tempWert = speicher.getWert();
+
+	}
     }
 
     /**
@@ -34,7 +33,51 @@ public class SpeicherTest {
      */
     @Test
     public void testIsHatWert() {
-	// fail("Not yet implemented");
+	assertFalse(speicher.isHatWert());
+    }
+
+    /**
+     * Test method for {@link producerconsumer.Speicher#setWert(int)}.
+     */
+    @Test
+    public void testSetWert() {
+
+	Thread worker = new Thread(new Runnable() {
+	    public void run() {
+		speicher.setWert(5); // Testen ob der Wert richtig geschrieben wird
+		speicher.setWert(2); // Test ob der erste Wert ueberschrieben wird
+	    }
+	});
+	worker.start();
+	try {
+	    Thread.sleep(100);
+	} catch (InterruptedException e) {
+	    e.printStackTrace();
+	}
+	// testen ob der Thread wartet, da bereits ein Wert geschrieben wurde
+	assertEquals(Thread.State.WAITING, worker.getState());
+
+    }
+
+    
+    //funktioniert noch nicht der Wert wurde auf 5 gesetzt
+    
+    /**
+     * Test method for {@link producerconsumer.Speicher#getWert()}.
+     */
+    @Test
+    public void testGetWert() {
+	
+	Runner runner = new Runner(); 
+	Thread worker2 = new Thread (runner);
+		
+
+	worker2.start();
+//	int wert = runner.getTempWert();
+	
+	
+	assertEquals(1, runner.getTempWert()); // Test ob der richtige Wert zurueckgegeben wird
+	assertFalse(speicher.isHatWert()); // Test ob der Wert abgeholt wurde
     }
 
     /**
@@ -42,7 +85,7 @@ public class SpeicherTest {
      */
     @Test
     public void testIsLast() {
-	// fail("Not yet implemented");
+	assertFalse(speicher.isLast());
     }
 
     /**
@@ -50,7 +93,8 @@ public class SpeicherTest {
      */
     @Test
     public void testSetLast() {
-	// fail("Not yet implemented");
+	speicher.setLast(true);
+	assertTrue(speicher.isLast());
     }
 
 }
